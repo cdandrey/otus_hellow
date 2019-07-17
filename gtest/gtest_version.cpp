675c7b876412653edb8fa0d559ade38772f11002
Andrey <cdandrey@gmail.com>
2019-07-17 04:13:39 +0300

    Split repo to subdir

---
+++ gtest/gtest_version.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+#include "gtest/gtest.h"
+
+TEST_F(gtest_version_case,gtest_version){
+
+    ASSERT_TRUE(version() > 0);
+}
+
+int main (int argc,char **argv)
+{
+    ::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
